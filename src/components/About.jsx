a5c47802d8455f04db818e55d5a27bdd8e0bd1aa
Kassim <kassimrooney@gmail.com>
2025-04-11 11:03:18 +0300

    Completed jsx lab

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { image } from "../data/data";
+
+function About() {
+  return (<div id="about">About
+        <h2>About Me</h2>
+        <p>They call me Shallipopi!</p>
+        <img src={ image } alt="I made this" />
+  </div>);
+}
+
+export default About;
